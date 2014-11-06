406b0054c9d03899349fbf15f42b86b42f266a92
Jan Thieß Brockmann <jt.brockmann@web.de>
2014-11-06 13:34:32 +0100

    Revise GameScene default screen

--- EGGE2D_GameScene.java
+++ EGGE2D_GameScene.java
@@ -38,8 +38,8 @@ public class EGGE2D_GameScene {
 	private void drawLogo(int width, int height) {
 		if(objects.size()==0) {
 			int size = 4;
-			int x = 4;
-			int y = 4;
+			int x = width/2;
+			int y = height/2;
 			int[][] lines = {
 					{0,1,2,3,4,7,8,10,13,14,16,18,19,20,21,22,28,29,30,31,33,34,35,36},
 					{0,6,9,10,12,15,16,18,29,33,37},
@@ -50,12 +50,12 @@ public class EGGE2D_GameScene {
 				int qint = Arrays.asList(lines).indexOf(q);
 				for(int p: q) {
 					addObject("_"+p+"_"+qint, new EGGE2D_GameObject());
-					getObject("_"+p+"_"+qint).absPosition(p*size*2+x, qint*size*2+y);
+					getObject("_"+p+"_"+qint).absPosition(p*size*2+x-18*2*size, qint*size*2+y-5*4);
 					getObject("_"+p+"_"+qint).absScale(size, size);
 				}
 			}
 			addObject("_default", new EGGE2D_GameObject());
-			getObject("_default").absPosition(width/2, height/2);
+			getObject("_default").absPosition(width/2, height/2-8*size*2);
 			
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
